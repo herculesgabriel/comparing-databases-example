ac6ea0a5282d827c473568d665312c3f508a5e7c
Hercules Gabriel <herculesgabriel00@gmail.com>
2022-09-05 11:13:29 -0300

    feat: change comparing function for read data has changed

--- index.ts
+++ index.ts
@@ -1,14 +1,20 @@
 import { resolve } from 'node:path';
 import { compareData, saveOnDisk, getIds } from './utils/storage';
 
-const POSTGRES_SOURCE_FILE_PATH = resolve('./results/dbs/testingPg.json');
-const MONGO_SOURCE_FILE_PATH = resolve('./results/dbs/mongo_transactions.json');
-const RESULT_FILE_PATH = resolve('./results/testingThings.json');
+const POSTGRES_SOURCE_FILE_PATH = resolve('./results/dbs/transactionsPg');
+const MONGO_SOURCE_FILE_PATH = resolve('./results/dbs/transactionsMongo');
+const RESULT_FILE_PATH = resolve('./results/comparison');
 
 (async () => {
-  console.log('---> Starting');
-  // const result = await compareData(POSTGRES_SOURCE_FILE_PATH, MONGO_SOURCE_FILE_PATH);
-  const result = await getIds(POSTGRES_SOURCE_FILE_PATH, MONGO_SOURCE_FILE_PATH);
+  console.info('---> Starting');
+  console.time('Comparison time');
+
+  const result = await compareData(POSTGRES_SOURCE_FILE_PATH, MONGO_SOURCE_FILE_PATH);
+
+  console.timeEnd('Comparison time');
+  console.info(result);
+
   saveOnDisk(result, RESULT_FILE_PATH);
-  console.log('---> Finished');
+
+  console.info('---> Finished');
 })();
--- utils/storage.ts
+++ utils/storage.ts
@@ -1,86 +1,78 @@
-import { createReadStream, writeFileSync, readFileSync } from 'node:fs';
-import { streamArray } from 'stream-json/streamers/StreamArray';
+import { createReadStream, readFileSync, createWriteStream } from 'node:fs';
 import { chain } from 'stream-chain';
-import { parser } from 'stream-json';
 
 export function readFile(path: string) {
   const file = readFileSync(path, 'utf-8');
   return JSON.parse(file);
 }
 
-export function saveOnDisk(data: unknown, path: string) {
+export function saveOnDisk(data: string[], path: string) {
   console.info('- Saving results to disk...');
 
-  writeFileSync(path, JSON.stringify(data, null, 2), 'utf-8');
-  const fileName = path.split('/').pop();
+  const writable = createWriteStream(path, 'utf-8');
+  for (const id of data) {
+    writable.write(`${id}\n`);
+  }
+  writable.close();
 
+  const fileName = path.split('/').pop();
   console.info(`- Saved ${fileName} to disk.`);
 }
 
 const readSecondFile = async (mongoPath: string, postgresId: string) =>
   new Promise(resolve => {
-    const parserChain = chain([createReadStream(mongoPath), parser(), streamArray()]);
+    const parserChain = chain([createReadStream(mongoPath)]);
 
     let found = false;
 
     parserChain.on('data', async data => {
       parserChain.pause();
 
-      if (data.key % 100000 === 0) {
-        console.info('Mongo:', data.key);
+      const parsedData = data.toString();
+      const mongoIds = parsedData.split('\n');
+
+      for (const mongoId of mongoIds) {
+        if (mongoId === postgresId) {
+          found = true;
+          parserChain.emit('end');
+        }
       }
 
-      const mongoId = data.value._id;
-      if (mongoId === postgresId) {
-        found = true;
-        parserChain.emit('end');
-      } else {
-        parserChain.resume();
-      }
+      parserChain.resume();
     });
 
     parserChain.on('end', () => resolve(found));
   });
 
-export const compareData = async (postgresPath: string, mongoPath: string) =>
+export const compareData = async (
+  postgresPath: string,
+  mongoPath: string,
+): Promise<string[]> =>
   new Promise(resolve => {
     const result: string[] = [];
+    let count = 0;
 
     const parserChain = chain([
       createReadStream(postgresPath),
-      parser(),
-      streamArray(),
-      async data => {
+      async (data: Buffer) => {
         parserChain.pause();
 
-        console.info('PG:', data.key);
+        const parsedData = data.toString();
+        const postgresIds = parsedData
+          .split('\n')
+          .map((each: string) => each.replace('\r', ''));
 
-        const postgresId = data.value.id;
-        const exists = await readSecondFile(mongoPath, postgresId);
-        if (!exists) {
-          result.push(postgresId);
+        for (const postgresId of postgresIds) {
+          console.info('PG ID:', count);
+          count++;
+
+          const exists = await readSecondFile(mongoPath, postgresId);
+          if (!exists) {
+            result.push(postgresId);
+          }
         }
 
         parserChain.resume();
-      },
-    ]);
-
-    parserChain.on('end', () => resolve(result));
-  });
-
-export const getIds = async (path: string) =>
-  new Promise(resolve => {
-    const result: string[] = [];
-
-    const parserChain = chain([
-      createReadStream(path),
-      parser(),
-      streamArray(),
-      data => {
-        if (data.key % 100000 === 0) {
-          console.info('Current ID:', data.key);
-        }
-        result.push(data.value.id);
       },
     ]);
 
